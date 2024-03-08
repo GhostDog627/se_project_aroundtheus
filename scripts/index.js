4bc4509a09055726cb29adba95750ff7dff6a724
TuckerJudy <tucker.judymedia@gmail.com>
2024-03-07 19:19:44 -0500

    project 4 stage 1 updates

---
+++ scripts/index.js
@@ -0,0 +1,30 @@
+const initialCards = [
+{  name: "back of head portrait",
+   link: https:"//unsplash.com/photos/a-man-with-tattoos-covering-his-face-and-hands-behind-his-head-10TQYfgWnQI",
+},
+
+{
+  name:"old cars",
+  link:"https://unsplash.com/photos/a-car-is-parked-in-front-of-a-house-yEYravYsZkU",
+},
+
+{
+  name:"nightime japan",
+  link:"https://unsplash.com/photos/a-woman-walking-down-a-street-holding-an-umbrella-5KRVZp_23a0",
+},
+
+{
+  name:"field",
+  link:"https://unsplash.com/photos/a-grassy-field-with-a-fence-in-the-background-NdK5-yABH2c",
+},
+
+{
+  name:"hiking",
+  link:"https://unsplash.com/photos/a-person-walking-up-a-hill-on-a-sunny-day-JM0VeViZPo4",
+},
+
+{
+  name:"motorcycle in mountains",
+  link:"https://unsplash.com/photos/black-motorcycle-on-brown-dirt-road-during-daytime--P-YV9aTyHE",
+},
+];
